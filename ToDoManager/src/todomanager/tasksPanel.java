c1145256a4083b314217858cfb1cfb4177542692
Kristian <Kristian@212.25.147.170>
2013-02-05 12:13:55 +0100

    itemPanel class and taskspanel class by Kristian and Kristoffer

---
+++ ToDoManager/src/todomanager/tasksPanel.java
@@ -0,0 +1,41 @@
+package todomanager;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import mockClasses.item;
+/**
+ *
+ * @author Kristian Johansson and Kristoffer Wass
+ */
+class tasksPanel extends JPanel{
+    
+    private itemPanel itemPanel;
+    
+    public tasksPanel(){
+        setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+    }
+    
+    /**
+     * Method that updates the GUI, takes a list of items as argument
+     * @param items 
+     */
+    public void updatePanel(ArrayList<item> items){
+        for(int i=0;i<items.size();i++){
+            addItems(items.get(i));
+        }
+    }
+    
+    /**
+     * Create an itemPanel using the input item and adds this panel to
+     * the tasksPanel
+     * @param item 
+     */
+    private void addItems(item item){
+        itemPanel = new itemPanel(item.getName());
+        itemPanel.setBackground(Color.red);
+        itemPanel.setAlignmentX(0);
+        this.add(itemPanel);
+    }
+}
