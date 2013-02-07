3273eb378e8a2594544e513d385cceedd1fddfd9
Emil <Emil@nl119-150-59.student.uu.se>
2013-02-07 12:33:30 +0100

    Added JavaDoc

--- ToDoManager/src/todomanager/ToDoItem.java
+++ ToDoManager/src/todomanager/ToDoItem.java
@@ -1,23 +1,41 @@
 package todomanager;
 
 /**
- *
- * @author Emil
+
+ * @author Emil Björling
  */
 public class ToDoItem {
 
+/** a ToDoItem's title **/    
     private String title;
+/** a ToDoItem's description **/    
     private String description;
 
-    public ToDoItem(String titl, String desc) {
-        title = titl;
+/**
+ * Constructor for a ToDoItem
+ * @param titl the wanted title of a ToDoItem 
+ * @param desc the wanted description a ToDoItem 
+ */    
+    
+    public ToDoItem(String tit, String desc) {
+        title = tit;
         description = desc;
     }
 
+/**
+ * Returns the title of a ToDoItem
+ *
+ * @return title a string containing title of a ToDoItem 
+ */    
     public String getTitle() {
         return title;
     }
 
+/**
+ * Returns the description of a ToDoItem
+ *
+ * @return description a string containing the title of a ToDoItem 
+ */          
     public String getDescription() {
         return description;
     }
