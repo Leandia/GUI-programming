71112a40b3d81ee5682ced731ce1e20fa4949bb0
Daniel Gustafsson <>
2013-02-19 21:51:30 +0100

    Changed the DB to accept categories to. It is now possible to get an ArrayList<Category> out from a call to getCategories. These categories will have all items that belongs to them in their own lists.
    Added two constructors to Category for the above to work.

--- ToDoManager/src/todomanager/Category.java
+++ ToDoManager/src/todomanager/Category.java
@@ -6,39 +6,63 @@ import java.util.ArrayList;
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Emil
  */
 public class Category {
+
+    private String categoryTitle;
+    private final int id;
+    private ArrayList<ToDoItem> toDoItems = new ArrayList<>();
+
+    /**
+     * Create a category with given number and name and a set list of items.
+     * @param id The number to identify this category.
+     * @param title The name of this category.
+     * @param list The list of items in this category.
+     */
+    public Category(int id, String title, ArrayList<ToDoItem> list) {
+        this.id = id;
+        this.categoryTitle = title;
+        this.toDoItems = list;
+    }
     
-  private String categoryTitle;
-  private int id;
-  private ArrayList<ToDoItem> toDoItems = new ArrayList<>();
-  
-  public Category(String title){
-      this.categoryTitle = title;
-      this.toDoItems = null;
-  }
-  
-  public void setCategoryTitle(String title){
-      this.categoryTitle = title;
-  }
-  
-  public void addToDoItem(ToDoItem item){
-      toDoItems.add(item);
-  }
-  
-  public String getCategoryTitle(){
-      return this.categoryTitle;
-  }
-  
-  public int getId(){
-      return this.id;
-  }
-  
-  public ArrayList getToDoItems(){
-      return this.toDoItems;
-  }
+    /**
+     * Create a category with given number and name.
+     * @param id The number to identify this category.
+     * @param title The name of this category.
+     */
+    public Category(int id, String title) {
+        this(id, title, null);
+    }
+
+    /**
+     * @deprecated Do not use, use Category(int, String) or 
+     * Category(int, String, ArrayList<ToDoItem> instead.
+     * @param title Title of the category.
+     */
+    public Category(String title) {
+        this(0, title, null);
+    }
+
+    public void setCategoryTitle(String title) {
+        this.categoryTitle = title;
+    }
+
+    public void addToDoItem(ToDoItem item) {
+        toDoItems.add(item);
+    }
+
+    public String getCategoryTitle() {
+        return this.categoryTitle;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public ArrayList getToDoItems() {
+        return this.toDoItems;
+    }
 }
