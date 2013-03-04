cf255701611c047077b756f4e99b325a84a9fb1e
Daniel Gustafsson <>
2013-03-04 19:40:29 +0100

    Documentation

--- ToDoManager/src/todomanager/ImagePanel.java
+++ ToDoManager/src/todomanager/ImagePanel.java
@@ -8,7 +8,8 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 /**
- *
+ * Panel for displaying an image. It draws the image straight on the panel, 
+ * below any items added to the panel.
  * @author Daniel
  */
 public class ImagePanel extends JPanel {
@@ -18,6 +19,10 @@ public class ImagePanel extends JPanel {
     public ImagePanel() {
     }
 
+    /**
+     * Method to set the image to be drawn on the panel.
+     * @param address A valid address to an image.
+     */
     public void setPicture(String address) {
         try {
             image = ImageIO.read(new File(address));
