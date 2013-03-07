e290938b2d8f54c28dd3f0b497922e5e11e8de0a
Daniel Gustafsson <>
2013-03-07 17:54:31 +0100

    Small changes to theme

--- ToDoManager/src/todomanager/Theme.java
+++ ToDoManager/src/todomanager/Theme.java
@@ -1,5 +1,7 @@
 package todomanager;
 
+import java.awt.Color;
+import javax.swing.plaf.ColorUIResource;
 import javax.swing.plaf.metal.DefaultMetalTheme;
 
 /**
@@ -8,7 +10,25 @@ import javax.swing.plaf.metal.DefaultMetalTheme;
  */
 public class Theme extends DefaultMetalTheme {
 
+    private String p1 = "#FFFFFF";
+    private ColorUIResource primary1 = new ColorUIResource(Color.decode(p1));
+
     public Theme() {
         super();
     }
+
+    @Override
+    protected ColorUIResource getPrimary1() {
+        return primary1;
+    }
+    @Override
+    protected ColorUIResource getPrimary2() {
+        return primary1;
+    }
+    @Override
+    protected ColorUIResource getPrimary3() {
+        return primary1;
+    }
+    
+    
 }
